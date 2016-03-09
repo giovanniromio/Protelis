163a02f0605203c73457d6eae178cc5d017c1110
Studente <giovanni.romio4@gmail.com>
2016-03-09 18:31:02 +0100

    Couldn't resolve reference to EObject 'fill'

--- src/main/java/org/protelis/lang/datatype/TupleHelper.java
+++ src/main/java/org/protelis/lang/datatype/TupleHelper.java
@@ -1,11 +1,13 @@
 package org.protelis.lang.datatype;
 
-import java.util.List;
 
+import java.util.List;
 import org.protelis.lang.datatype.impl.ArrayTupleImpl;
 
 
-public class TupleHelper{
+
+public class TupleHelper {
+	
 	public static Tuple create(final List<?> l) {
         return create(l.toArray());
     }
@@ -13,6 +15,22 @@ public class TupleHelper{
 	public static Tuple create(Object... l) {
 		return new ArrayTupleImpl(l); 
     }
-
+	
+	public static Tuple fill(Object value, int length) {
+        return new ArrayTupleImpl(value, length);
+    }
+	
+	public static Tuple union(Tuple t1, Tuple t2) {
+        return t1.union(t2);
+    }
+	
+	public static Tuple intersection(Tuple t1, Tuple t2) {
+        return t1.intersection(t2);
+    }
+	
+	public static Tuple subtract(Tuple t1, Tuple t2) {
+        return t1.subtract(t2);
+    }
+	
 	
 }
